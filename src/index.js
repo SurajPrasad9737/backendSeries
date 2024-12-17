c9fe9ce884414997c57e55cd226c4dcb839486a1
SurajPrasad9737 <surajprasad311004@gmail.com>
2024-12-17 09:29:06 +0530

    utils created

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@
 import dotenv from "dotenv";
 
 import connectDB from "../src/db/index.js";
+import { app } from "./app.js";
 
-connectDB();
 
 dotenv.config({path:'./env'});
+
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server is listening on the port : ${process.env.PORT}`)
+   });
+
+   app.on('error',(error)=>{
+    console.log("Mongodb error : ",error);
+    throw error;
+   })
+   
+})
+.catch((err)=>{
+    console.log("Mongodb Error : ",err);
+    process.exit(1);
+})
+
 
 
 // import express from "express";
